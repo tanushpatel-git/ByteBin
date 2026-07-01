44465c5b12059076ab0c9f1a5577fb1730b59d08
tanushpatel-git <tanush000patel@gmail.com>
2026-07-02 03:13:13 +0530

    server delete and re initial

--- Server/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const {Router} =require("express"); 
-const authRouter=Router(); 
-const {registerUser, loginUser, logoutUser}=require("../controllers/auth.controller");
-
-authRouter.post("/register",registerUser);
-authRouter.post("/login",loginUser);
-authRouter.post("/logout",logoutUser);
-
-module.exports=authRouter;
--- Server/src/routes/task.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {create,getTitles} = require("../controllers/workspace.controller");
-
-router.get("/titles", getTitles);
-router.post("/create-task", create);
-
-module.exports = router;
