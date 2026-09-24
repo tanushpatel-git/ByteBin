343b25327e6fed7bb18e632de59eb258934b00ad
tanushpatel-git <tanush000patel@gmail.com>
2026-09-24 16:24:01 +0530

    style(navbar): add translucent glass background

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
   return (
     <header
       id="navbar"
-      className="fixed left-0 top-0 z-20 w-full transition-transform duration-300"
+      className="fixed left-0 top-0 z-20 w-full bg-white/10 transition-transform duration-300"
     >
       <div className="mx-auto flex h-[72px] max-w-[1200px] items-center justify-between px-6">
         <Logo />
