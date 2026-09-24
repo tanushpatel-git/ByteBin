d1aee5a40eeb1ae5662e0479ae57441cddc7dac0
tanushpatel-git <tanush000patel@gmail.com>
2026-09-24 02:48:00 +0000

    feat(ui): add TaskGo logo mark

---
+++ Client/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center gap-2.5">
+      <div className="relative h-7 w-7">
+        <span className="absolute left-0 top-2 h-4 w-4 rotate-45 rounded-[5px] bg-[#635BFF]" />
+        <span className="absolute left-3.5 top-0 h-4 w-4 rotate-45 rounded-[5px] bg-[#635BFF]" />
+        <span className="absolute bottom-0 left-3.5 h-4 w-4 rotate-45 rounded-[5px] bg-[#635BFF]" />
+      </div>
+    </div>
+  );
+}
