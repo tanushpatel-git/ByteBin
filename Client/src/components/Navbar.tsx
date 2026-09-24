ffba70b7df1d60fcd9a2af56108e674b6d1bb100
tanushpatel-git <tanush000patel@gmail.com>
2026-09-24 16:24:24 +0530

    feat(navbar): reveal navbar on upward scroll

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -19,6 +19,8 @@ export default function Navbar() {
 
       if (currentScrollY > lastScrollY && currentScrollY > 100) {
         header.style.transform = "translateY(-100%)";
+      } else {
+        header.style.transform = "translateY(0)";
       }
 
       lastScrollY = currentScrollY;
