0cd61165f78131589ff58a2c3d94f01c598c6ee9
tanushpatel-git <tanush000patel@gmail.com>
2026-09-24 01:00:00 +0000

    feat(scroll): sync Lenis with GSAP ScrollTrigger ticker

--- Client/src/components/SmoothScroll.tsx
+++ Client/src/components/SmoothScroll.tsx
@@ -2,6 +2,10 @@
 
 import { useEffect } from "react";
 import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function SmoothScroll({
   children,
@@ -15,7 +19,17 @@ export default function SmoothScroll({
       syncTouch: true,
     });
 
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const update = (time: number) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(update);
+    gsap.ticker.lagSmoothing(0);
+
     return () => {
+      gsap.ticker.remove(update);
       lenis.destroy();
     };
   }, []);
