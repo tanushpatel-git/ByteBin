e905885df9948d8477eda829e0c91bfaee734b54
tanushpatel-git <tanush000patel@gmail.com>
2026-09-24 07:00:00 +0000

    feat(cta): add CTA heading and support copy

--- Client/src/components/CTA.tsx
+++ Client/src/components/CTA.tsx
@@ -5,7 +5,14 @@ export default function CTA() {
         <div className="pointer-events-none absolute -left-10 -top-10 h-32 w-32 rounded-full bg-white/10 blur-xl" />
         <div className="pointer-events-none absolute -right-10 -bottom-10 h-32 w-32 rounded-full bg-white/10 blur-xl" />
 
-        <div className="relative"></div>
+        <div className="relative">
+          <h2 className="text-lg font-bold">Ready to Get Started?</h2>
+          <p className="mt-1 text-[9px] text-white/80">
+            Join thousands of teams already using TaskGo to work smarter
+            <br />
+            and achieve more.
+          </p>
+        </div>
 
         <div className="relative text-center"></div>
       </div>
